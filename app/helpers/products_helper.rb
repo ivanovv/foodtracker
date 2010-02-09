83bf39c87c400e46ea4afd03a36683b418131f92
root <root@rails.(none)>
2010-02-09 20:26:51 +0000

    just checked in

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,9 @@
 module ProductsHelper
+   
+  def hidden_div_if(condition, attributes = {}, &block)
+    if condition
+      attributes["style"] = "display: none"
+    end
+    content_tag("div", attributes, &block)
+  end
 end
