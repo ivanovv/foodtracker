e33add9b41e51a3ce5e79ab9de7bc5e31d421ece
root <root@rails.(none)>
2010-02-09 06:20:33 +0000

    Borrowed some useful methods from the template (mostly authentication)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
-  filter_parameter_logging :password, :password_confirmation
-  helper_method :current_user_session, :current_user
+  filter_parameter_logging :password, :password_confirmation, :confirm_password
+  helper_method :current_user_session, :current_user, :logged_in?
+  
 
   private
+  
+    def logged_in?
+    !current_user_session.nil?
+  end
     
   def current_user_session
     return @current_user_session if defined?(@current_user_session)
@@ -19,6 +24,26 @@ class ApplicationController < ActionController::Base
   def current_user
     return @current_user if defined?(@current_user)
     @current_user = current_user_session && current_user_session.user
+  end
+  
+  def require_user
+    unless current_user
+      store_location
+      flash[:notice] = "You must be logged in to access this page."
+      redirect_to new_user_session_url
+    end
+  end
+  
+  def require_no_user
+    if current_user
+      store_location
+      flash[:notice] = "You must be logged out to access this page"
+      return false
+    end
+  end
+  
+  def store_location
+    session[:return_to] = request.request_uri
   end
 
 end
