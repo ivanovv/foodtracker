24fa5d6fb2a5a1c6c9ab393c4ecaea45ef3ee867
root <root@rails.(none)>
2010-02-09 06:35:00 +0000

    added calory_lines as children; added search by product name

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,17 @@ class Product < ActiveRecord::Base
   attr_accessible :category_id, :name, :water, :protein, :fat, :carbohydrate, :energy
   
   belongs_to :category
+  has_many :calory_lines
   
   validates_presence_of :name, :energy, :category_id
   validates_uniqueness_of :name
   validates_numericality_of :water, :protein, :fat, :carbohydrate, :energy
+
+  def self.search(product_name)
+    if product_name
+      find(:all, :conditions => ['name like ?', "%#{product_name}%"])
+    else
+      find(:all)
+    end
+  end
 end
