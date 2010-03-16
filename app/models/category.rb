8fda6a70c69ba45ce6670dd17701104461297e3f
ViC <vic@ubuntu.ubuntu-domain>
2010-03-16 09:00:03 +0300

    search select should return unique values

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,18 +1,19 @@
 class Category < ActiveRecord::Base
   attr_accessible :name
-  
+
   has_many :products
-  
+
   validates_presence_of :name
   validates_uniqueness_of :name
-  
+
   def self.search_by_product_name(product_name)
     if product_name
       product_name.downcase!
-      find_by_sql( "select categories.* from categories inner join products " +
+      find_by_sql( "select distinct categories.* from categories inner join products " +
       "on products.category_id = categories.id where products.name like '%#{product_name}%'")
     else
       find(:all)
     end
   end
 end
+
