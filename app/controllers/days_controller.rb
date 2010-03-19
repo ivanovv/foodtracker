5ddd3831867a94ce1a4d3829108a95435a271389
ViC <vic@ubuntu.ubuntu-domain>
2010-03-20 02:06:27 +0300

    day: index action - search via options; create action - redirect if location was stored; find_by_date; total_calories and metabolic_rate added to the model; translation

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,35 +1,48 @@
 class DaysController < ApplicationController
   before_filter :require_user
+
   def index
-    @days = Day.all
+    options = {
+      :order => 'enter_date DESC',
+      #:per_page => 10,
+      #:page => params[:page]
+      :conditions => [ "user_id = :user", {:user => current_user.id} ]
+    }
+
+    if params[:search]
+      options[:conditions] = [
+      "name LIKE :search", {:search => "%#{params[:search]}%"}
+      ]
+    end
+    @days = Day.all(options)
   end
-  
+
   def show
-    @day = Day.find_by_enter_date(params[:id].to_date)
+    @day = Day.find_by_date(params[:id].to_date)
+    user = current_user
   end
-  
+
   def new
-    @day = Day.new
-    @day.user_id = current_user.id
+    @day = Day.new(:user => current_user)
   end
-  
+
   def create
     @day = Day.new(params[:day])
-    @day.user_id = current_user.id
+    @day.user = current_user
     if @day.save
       flash[:notice] = "Successfully created day."
-      redirect_to @day
+      redirect_to (session[:return_to] || @day)
     else
       render :action => 'new'
     end
   end
-  
+
   def edit
-    @day = Day.find_by_enter_date(params[:id].to_date)
+    @day = Day.find_by_date(params[:id].to_date)
   end
-  
+
   def update
-    @day = Day.find_by_enter_date(params[:id].to_date)
+    @day = Day.find_by_date(params[:id].to_date)
     if @day.update_attributes(params[:day])
       flash[:notice] = "Successfully updated day."
       redirect_to @day
@@ -37,11 +50,17 @@ class DaysController < ApplicationController
       render :action => 'edit'
     end
   end
-  
+
   def destroy
-    @day = Day.find_by_enter_date(params[:id].to_date)
+    @day = Day.find_by_date(params[:id].to_date)
     @day.destroy
     flash[:notice] = "Successfully destroyed day."
     redirect_to days_url
   end
+
+  private
+  def day
+    @day ||= Day.find_by_date(params[:id].to_date)
+  end
 end
+
--- app/models/day.rb
+++ app/models/day.rb
@@ -1,15 +1,41 @@
 class Day < ActiveRecord::Base
   attr_accessible :enter_date, :weight
-  
+
   validates_presence_of :enter_date
   validates_uniqueness_of :enter_date, :scope=> [:user_id]
   validates_presence_of :weight
   validates_numericality_of :weight
-  
-  belongs_to :user  
+
+  belongs_to :user
   has_many :calory_lines
+
+  def self.find_by_date(date = Date.today)
+    find_by_enter_date_and_user_id(date, UserSession.find.user)
+  end
 
   def to_param
     "#{enter_date.year.to_s}-#{enter_date.month.to_s}-#{enter_date.day.to_s}"
   end
+
+  def to_s
+    to_param
+  end
+
+  def to_label
+    enter_date.to_param
+  end
+
+  def base_metabolic_rate
+    user.base_metabolic_rate(weight)
+  end
+
+  def total_calories
+    result = 0
+    calory_lines.each do |calory_line|
+      result += calory_line.total_calories
+    end
+    result
+  end
+
 end
+
