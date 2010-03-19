c66147bbbbf9034873a0a650d448014f4133744a
ViC <vic@ubuntu.ubuntu-domain>
2010-03-20 01:59:59 +0300

    index is created via table_helper day is taken into account in URLs day and product ids are loaded via filter translations

--- app/controllers/calory_lines_controller.rb
+++ app/controllers/calory_lines_controller.rb
@@ -1,32 +1,56 @@
 class CaloryLinesController < ApplicationController
+
+  before_filter :require_user, :get_day, :get_product
+
   def index
     if params[:day_id]
-      @day = Day.find_by_enter_date(params[:day_id].to_date)
       if @day
         @calory_lines = @day.calory_lines
       else
         redirect_to new_day_path
         @calory_lines = []
-      end	
+      end
     else
-      @calory_lines = CaloryLine.all
+      @calory_lines = CaloryLine.find(:all, :include => [:day, :product],
+        :conditions => ["days.user_id = :user_id", {:user_id => current_user.id}])
+
     end
   end
-  
+
   def show
     @calory_line = CaloryLine.find(params[:id])
   end
-  
+
   def new
     @calory_line = CaloryLine.new
-    @day = Day.find_by_enter_date(Date.today)
-    if @day
-      @calory_line.day_id = @day.id
+    @user_days = current_user.days
+    if params[:day_id]
+      if @day
+        @calory_line.day_id = @day.id
+      else
+        store_location
+        redirect_to new_day_path
+      end
+    else
+      if @today
+        @calory_line.day_id = @today.id
+      end
+    end
+
+    if params[:product_id]
+      if @product
+        @calory_line.product_id = @product.id
+        @calory_line.energy = @product.energy
+      else
+        flash[:notice] = "Product not found!"
+        redirect_to products_path
+      end
     end
   end
-  
+
   def create
     @calory_line = CaloryLine.new(params[:calory_line])
+    @calory_line.energy ||= @calory_line.product.energy
     if @calory_line.save
       flash[:notice] = "Successfully created calory line."
       redirect_to @calory_line
@@ -34,11 +58,12 @@ class CaloryLinesController < ApplicationController
       render :action => 'new'
     end
   end
-  
+
   def edit
     @calory_line = CaloryLine.find(params[:id])
+    @user_days = current_user.days
   end
-  
+
   def update
     @calory_line = CaloryLine.find(params[:id])
     if @calory_line.update_attributes(params[:calory_line])
@@ -48,11 +73,32 @@ class CaloryLinesController < ApplicationController
       render :action => 'edit'
     end
   end
-  
+
   def destroy
     @calory_line = CaloryLine.find(params[:id])
     @calory_line.destroy
     flash[:notice] = "Successfully destroyed calory line."
-    redirect_to calory_lines_url
+
+    redirect_to @day ? day_calory_lines_path(@day) :calory_lines_url
   end
+
+  private
+
+  def get_day
+    @day, @today = nil, nil
+    if params[:day_id]
+      @day = Day.find_by_date(params[:day_id].to_date)
+    else
+      @today = Day.find_by_date()
+    end
+  end
+
+  def get_product
+    @product = nil
+    if params[:product_id]
+      @product = Product.find(params[:product_id])
+    end
+  end
+
 end
+
--- app/helpers/calory_lines_helper.rb
+++ app/helpers/calory_lines_helper.rb
@@ -1,3 +1,33 @@
 module CaloryLinesHelper
+
+  def calory_line_table
+    collection_table(@calory_lines, :class => 'app-table') do |t|
+      t.header.hide_when_empty = false
+      t.header.column :day, t('calory_lines.day'), :class => "text"
+      t.header.column :product, t('calory_lines.product'), :class => "text"
+      t.header.column :net_weight, t('calory_lines.net_weight'), :class => "text"
+      t.header.column :energy, t('calory_lines.energy'), :class => "text"
+      t.header.column :total_calories, t('calory_lines.total_calories'), :class => "text"
+
+      t.header.column :actions, ''
+
+      t.rows.alternate = :odd
+      t.rows.empty_caption = t('.no_calory_lines')
+      t.rows.each do |row, item, index|
+        row[:id] = "calory_line-#{item.id}"
+        row.day  item.day.enter_date.to_s
+        row.product item.product.name
+        row.net_weight item.net_weight
+        row.energy item.energy
+        row.total_calories item.total_calories
+        delete_url = @day ? day_calory_line_path(@day, item) : calory_line_path(item)
+        logger.debug delete_url
+        edit_url = @day ? edit_day_calory_line_path(@day, item) :  edit_calory_line_path(item)
+
+        row.actions table_actions(edit_url, delete_url, item), :class => "buttons"
+      end
+    end
+  end
+
 end
 
