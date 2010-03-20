44db6de98bf4c2660a87a1f29404e56078b135f0
ViC <vic@ubuntu.ubuntu-domain>
2010-03-20 11:25:57 +0300

    redirect to new day action after login if there is no day for today for that user

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -2,18 +2,21 @@ class UserSessionsController < ApplicationController
   def new
     @user_session = UserSession.new
   end
-  
+
   def create
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
       flash[:notice] = "Successfully logged in."
-      redirect_to (session[:return_to] || root_url)
+      day = Day.find_by_date()
+      logger.debug "session = " + session[:return_to].to_s
+      logger.debug "day = " + day.to_s
+      redirect_to (session[:return_to] || (day ? root_url : new_day_path))
       session[:return_to] = nil
     else
       render :action => 'new'
     end
   end
-  
+
   def destroy
     @user_session = UserSession.find
     @user_session.destroy
@@ -21,3 +24,4 @@ class UserSessionsController < ApplicationController
     redirect_to root_url
   end
 end
+
