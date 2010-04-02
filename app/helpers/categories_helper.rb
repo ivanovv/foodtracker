fa2d356fae8f7b8b2d64d39c4de87c9637240264
ViC <vic@ubuntu.ubuntu-domain>
2010-04-02 11:12:23 +0400

    category index table rows are clickable now and their color is changed to normal text (from links) ; rows color alternation

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -10,7 +10,8 @@ module CategoriesHelper
       t.rows.empty_caption = t('.no_categories')
       t.rows.each do |row, item, index|
         row[:id] = "category-#{item.id}"
-        row.name  item.name
+        row[:onclick] = "location.href =  '#{category_products_path(item)}'"
+        row.name  link_to item.name, category_products_path(item)
         row.actions table_actions(edit_category_path(item), category_path(item), item), :class => "buttons"
       end
     end
