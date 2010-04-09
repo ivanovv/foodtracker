a91b3659b124d238b5262f2c3da4c592372be971
ViC <vic@ubuntu.ubuntu-domain>
2010-04-09 15:10:42 +0400

    filter_parameter moved from app_controller to app.rb; set template engine to HAML

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
-  filter_parameter_logging :password, :password_confirmation, :confirm_password
+  #filter_parameter_logging :password, :password_confirmation, :confirm_password
   helper_method :current_user_session, :current_user, :logged_in?
 
 
--- config/application.rb
+++ config/application.rb
@@ -31,14 +31,15 @@ module Demo
     # config.i18n.default_locale = :de
 
     # Configure generators values. Many other options are available, be sure to check the documentation.
-    # config.generators do |g|
+    config.generators do |g|
     #   g.orm             :active_record
-    #   g.template_engine :erb
+      g.template_engine :haml
     #   g.test_framework  :test_unit, :fixture => true
-    # end
+    end
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters << :password
+    config.filter_parameters << [:password, :password_confirmation, :confirm_password]
+
   end
 end
 
