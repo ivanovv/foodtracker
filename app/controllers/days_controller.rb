b38a45b02e4c7b62c3da82e4dc8859c76710fc22
ViC <vic@ubuntu.ubuntu-domain>
2010-04-13 18:41:59 +0400

    best practices: use current_user.days instead of day.user_id = current_user.id

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -2,23 +2,11 @@ class DaysController < ApplicationController
   before_filter :require_user
 
   def index
-    options = {
-      :order => 'enter_date DESC',
-      #:per_page => 10,
-      #:page => params[:page]
-      :conditions => [ "user_id = :user", {:user => current_user.id} ]
-    }
-
-    if params[:search]
-      options[:conditions] = [
-      "name LIKE :search", {:search => "%#{params[:search]}%"}
-      ]
-    end
-    @days = Day.all(options)
+    @days = current_user.days.all(:order => 'enter_date DESC')
   end
 
   def show
-    @day = Day.find_by_date(params[:id].to_date)
+    @day = current_user.days.find_by_date(params[:id].to_date)
     user = current_user
   end
 
@@ -27,8 +15,7 @@ class DaysController < ApplicationController
   end
 
   def create
-    @day = Day.new(params[:day])
-    @day.user = current_user
+    @day = current_user.days.build(params[:day])
     if @day.save
       flash[:notice] = "Successfully created day."
       redirect_to (session[:return_to] || @day)
@@ -38,11 +25,11 @@ class DaysController < ApplicationController
   end
 
   def edit
-    @day = Day.find_by_date(params[:id].to_date)
+    @day = current_user.days.find_by_date(params[:id].to_date)
   end
 
   def update
-    @day = Day.find_by_date(params[:id].to_date)
+    @day = current_user.days.find_by_date(params[:id].to_date)
     if @day.update_attributes(params[:day])
       flash[:notice] = "Successfully updated day."
       redirect_to @day
@@ -52,7 +39,7 @@ class DaysController < ApplicationController
   end
 
   def destroy
-    @day = Day.find_by_date(params[:id].to_date)
+    @day = current_user.days.find_by_date(params[:id].to_date)
     @day.destroy
     flash[:notice] = "Successfully destroyed day."
     redirect_to days_url
@@ -60,7 +47,7 @@ class DaysController < ApplicationController
 
   private
   def day
-    @day ||= Day.find_by_date(params[:id].to_date)
+    @day ||= current_user.days.find_by_date(params[:id].to_date)
   end
 end
 
