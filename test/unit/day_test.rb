6e927bcd0acc38f1f30e6fc87a7d821d67a0913d
ViC <xuily@yandex.ru>
2010-05-04 00:32:54 +0400

    implemented feature - days index should show how much the user can eaat for that day (as metabolic norm - eaten)

--- test/unit/day_test.rb
+++ test/unit/day_test.rb
@@ -3,21 +3,21 @@ require 'test_helper'
 class DayTest < ActiveSupport::TestCase
 
   should_belong_to :user
+  should_validate_presence_of :user_id, :enter_date, :weight
   should_have_many :calory_lines
+  should_ensure_value_in_range :weight, 20..200
 
   should "new day not be valid" do
     assert !Day.new.valid?
   end
 
   should "return formatted enter date as to_param" do
-    d = Day.new
-    d.enter_date = Date.today
+    d = Day.new(:enter_date => Date.today)
     assert_equal d.to_param, Date.today.strftime("%Y-%m-%d"), "to_param returned smth different from formatted enter_date"
   end
 
   should "calculate base metabolic rate correctly" do
-    d = Day.new
-    d.weight = 60
+    d = Day.new(:weight => 60)
     d.user = users(:vic)
     assert_equal d.base_metabolic_rate, users(:vic).base_metabolic_rate(60), "weight was calculated incorrectly"
   end
@@ -28,6 +28,11 @@ class DayTest < ActiveSupport::TestCase
       (calory_lines(:two).net_weight.to_f / 100 * calory_lines(:two).energy), "total calories were calculated wrong"
   end
 
+  should "calc the amount of calories the user can still eat" do
+    d = Day.new(:weight => 60)
+    d.user = users(:vic)
+    assert_equal d.to_eat, d.base_metabolic_rate - d.total_calories, "to_eat returned something wrong"
+  end
 
 end
 
