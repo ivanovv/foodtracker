f4e5ada387a99d5f09fe1e15854245a87004901a
ViC <xuily@yandex.ru>
2010-05-04 14:58:46 +0400

    backported scopes from rils3 because they are used in products controller

--- app/models/category.rb
+++ app/models/category.rb
@@ -6,6 +6,9 @@ class Category < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :name
 
+  named_scope :by_name, order("name ASC")
+  named_scope :search_by_name, lambda { |name| where("name LIKE '%#{name}%'") }
+
   def self.search_by_product_name(product_name)
     if product_name
       find_by_sql( "select distinct categories.* from categories inner join products " +
