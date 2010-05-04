9c4eafea7199aa8ead35158ffd1c3c3d0eef2c16
ViC <xuily@yandex.ru>
2010-05-04 14:41:47 +0400

    some more validations for the user model (height, age, authlogic)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ActiveRecord::Base
   acts_as_authentic
 
   validates_presence_of :username, :email, :born, :height
-  validates_numericality_of :height
+  validates_numericality_of :height, :greater_than_or_equal_to => 130, :less_than_or_equal_to => 250
+
+  validate :born_date_cannot_be_120_years_ago
 
   has_many :days
 
@@ -22,6 +24,12 @@ class User < ActiveRecord::Base
 
   def to_s
     username
+  end
+
+  private
+
+  def born_date_cannot_be_120_years_ago
+    errors.add(:born, "can't be older than 120 years ago") if !born.blank? and 120.years.ago > born
   end
 
 end
