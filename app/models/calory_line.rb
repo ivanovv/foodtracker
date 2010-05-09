a5c56e760c97adb7b3cb58bb5ba7f23fc7dbb06e
ViC <xuily@yandex.ru>
2010-05-09 17:14:40 +0400

    user should be able to enter 3g of product

--- app/models/calory_line.rb
+++ app/models/calory_line.rb
@@ -5,7 +5,7 @@ class CaloryLine < ActiveRecord::Base
   belongs_to :day
 
   validates_presence_of :net_weight, :product_id, :day_id
-  validates_numericality_of :net_weight, :greater_than => 10, :less_than => 5000
+  validates_numericality_of :net_weight, :greater_than => 2, :less_than => 5000
 
   def self.get_by_user(user)
     find_by_sql( "select calory_lines.*, days.* from calory_lines " +
