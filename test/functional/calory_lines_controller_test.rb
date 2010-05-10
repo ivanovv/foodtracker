c74b89bc171447367a6388617436af195a3298f8
ViC <xuily@yandex.ru>
2010-05-10 13:06:07 +0400

    tests rewritten to shoulda style

--- test/functional/calory_lines_controller_test.rb
+++ test/functional/calory_lines_controller_test.rb
@@ -1,54 +1,82 @@
 require 'test_helper'
 
 class CaloryLinesControllerTest < ActionController::TestCase
-  def test_index
-    get :index
-    assert_template 'index'
-  end
-  
-  def test_show
-    get :show, :id => CaloryLine.first
-    assert_template 'show'
-  end
-  
-  def test_new
-    get :new
-    assert_template 'new'
-  end
-  
-  def test_create_invalid
-    CaloryLine.any_instance.stubs(:valid?).returns(false)
-    post :create
-    assert_template 'new'
-  end
-  
-  def test_create_valid
-    CaloryLine.any_instance.stubs(:valid?).returns(true)
-    post :create
-    assert_redirected_to calory_line_url(assigns(:calory_line))
-  end
-  
-  def test_edit
-    get :edit, :id => CaloryLine.first
-    assert_template 'edit'
-  end
-  
-  def test_update_invalid
-    CaloryLine.any_instance.stubs(:valid?).returns(false)
-    put :update, :id => CaloryLine.first
-    assert_template 'edit'
-  end
-  
-  def test_update_valid
-    CaloryLine.any_instance.stubs(:valid?).returns(true)
-    put :update, :id => CaloryLine.first
-    assert_redirected_to calory_line_url(assigns(:calory_line))
-  end
-  
-  def test_destroy
-    calory_line = CaloryLine.first
-    delete :destroy, :id => calory_line
-    assert_redirected_to calory_lines_url
-    assert !CaloryLine.exists?(calory_line.id)
+
+  context "logged in user" do
+    setup do
+      activate_authlogic
+      UserSession.create users(:vic)
+    end
+
+    should "see index" do
+      get :index
+      assert_template 'index'
+      assert_not_nil assigns(:calory_lines)
+      assert_not_nil assigns(:total_calories)
+    end
+
+    should "see show" do
+      get :show, :id => CaloryLine.first
+      assert_template 'show'
+      assert_not_nil assigns(:calory_line)
+    end
+
+    should "see new calory line" do
+      get :new
+      assert_template 'new'
+      assert_not_nil assigns(:calory_line)
+    end
+
+    should "not be able to create invalid calory line" do
+      CaloryLine.any_instance.stubs(:valid?).returns(false)
+      CaloryLine.any_instance.stubs(:energy).returns(100)
+      post :create
+      assert_template 'new'
+    end
+
+    should "be able to create valid calory line" do
+      CaloryLine.any_instance.stubs(:valid?).returns(true)
+      CaloryLine.any_instance.stubs(:energy).returns(100)
+      post :create
+      assert_redirected_to calory_line_url(assigns(:calory_line))
+    end
+
+    should "be able to edit calory line" do
+      get :edit, :id => CaloryLine.first
+      assert_template 'edit'
+      assert_not_nil assigns(:calory_line)
+      assert_not_nil assigns(:user_days)
+    end
+
+    should "not be able to update invalid calory line" do
+      CaloryLine.any_instance.stubs(:valid?).returns(false)
+      put :update, :id => CaloryLine.first
+      assert_template 'edit'
+    end
+
+    should "be able to update valid calory line" do
+      CaloryLine.any_instance.stubs(:valid?).returns(true)
+      put :update, :id => CaloryLine.first
+      assert_redirected_to calory_line_url(assigns(:calory_line))
+    end
+
+    context "destroy" do
+
+      should "be able to destroy calory line and redirected to calory_lines" do
+        calory_line = CaloryLine.first
+        delete :destroy, :id => calory_line
+        assert_redirected_to calory_lines_url
+        assert !CaloryLine.exists?(calory_line.id)
+      end
+
+      should "be able to destroy calory line and redirected to day_calory_lines" do
+        calory_line = CaloryLine.first
+        day = days(:one)
+        delete :destroy, :id => calory_line, :day_id => day.to_param
+        assert_redirected_to day_calory_lines_url(day)
+        assert !CaloryLine.exists?(calory_line.id)
+      end
+
+    end
   end
 end
--- test/functional/days_controller_test.rb
+++ test/functional/days_controller_test.rb
@@ -1,54 +1,65 @@
 require 'test_helper'
 
 class DaysControllerTest < ActionController::TestCase
-  def test_index
-    get :index
-    assert_template 'index'
+
+  context "logged in user" do
+    setup do
+      activate_authlogic
+      UserSession.create users(:vic)
+    end
+
+    should "index" do
+      get :index
+      assert_template 'index'
+    end
+
+    should "show" do
+      get :show, :id => Day.first.to_param
+      assert_template 'show'
+    end
+
+    should "new" do
+      get :new
+      assert_template 'new'
+    end
+
+    should "create_invalid" do
+      Day.any_instance.stubs(:valid?).returns(false)
+      post :create
+      assert_template 'new'
+    end
+
+    should "create_valid" do
+      Day.any_instance.stubs(:valid?).returns(true)
+      Day.any_instance.stubs(:to_param).returns("2010-01-01")
+      post :create
+      assert_redirected_to day_url(assigns(:day))
+    end
+
+    should "edit" do
+      get :edit, :id => Day.first.to_param
+      assert_template 'edit'
+    end
+
+    should "update_invalid" do
+      Day.any_instance.stubs(:valid?).returns(false)
+      put :update, :id => Day.first.to_param
+      assert_template 'edit'
+    end
+
+    should "update_valid" do
+      Day.any_instance.stubs(:valid?).returns(true)
+      put :update, :id => Day.first.to_param
+      assert_redirected_to day_url(assigns(:day))
+    end
+
+    should "destroy" do
+      day = Day.first
+      delete :destroy, :id => day.to_param
+      assert_redirected_to days_url
+      assert !Day.exists?(day.id)
+    end
+
   end
-  
-  def test_show
-    get :show, :id => Day.first
-    assert_template 'show'
-  end
-  
-  def test_new
-    get :new
-    assert_template 'new'
-  end
-  
-  def test_create_invalid
-    Day.any_instance.stubs(:valid?).returns(false)
-    post :create
-    assert_template 'new'
-  end
-  
-  def test_create_valid
-    Day.any_instance.stubs(:valid?).returns(true)
-    post :create
-    assert_redirected_to day_url(assigns(:day))
-  end
-  
-  def test_edit
-    get :edit, :id => Day.first
-    assert_template 'edit'
-  end
-  
-  def test_update_invalid
-    Day.any_instance.stubs(:valid?).returns(false)
-    put :update, :id => Day.first
-    assert_template 'edit'
-  end
-  
-  def test_update_valid
-    Day.any_instance.stubs(:valid?).returns(true)
-    put :update, :id => Day.first
-    assert_redirected_to day_url(assigns(:day))
-  end
-  
-  def test_destroy
-    day = Day.first
-    delete :destroy, :id => day
-    assert_redirected_to days_url
-    assert !Day.exists?(day.id)
-  end
+
 end
